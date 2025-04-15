bbe1e1437d371c1f56cffb0a52ca67154dfdfae7
Ricardo <138608018+ric-projects@users.noreply.github.com>
2025-04-14 21:56:32 -0700

    Add .env example for setting up project

--- controllers/messageHandler.js
+++ controllers/messageHandler.js
@@ -1,11 +1,20 @@
 const pool = require("../db/pool");
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
 
-exports.postNewMessage = async (req, res) => {
-  const { title, msg } = req.body;
-  await db.postNewMsg(title, msg, res.locals.currentUser.user_id);
-  res.redirect("/");
-};
+const validateMessage = [
+  body("title").trim().isAlphanumeric().withMessage(`Invalid chars.`),
+  body("msg").trim(),
+];
+
+exports.postNewMessage = [
+  validateMessage,
+  async (req, res) => {
+    const { title, msg } = req.body;
+    await db.postNewMsg(title, msg, res.locals.currentUser.user_id);
+    res.redirect("/");
+  },
+];
 
 exports.delMessage = async (req, res) => {
   const msg_id = Number(req.params.msgid);
